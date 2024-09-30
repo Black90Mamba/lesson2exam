9f2cb843f9905542653e8a3b8f1e570b979ac223
Petra Volkova <volkovapetra17@gmail.com>
2024-09-30 20:30:22 +0200

    dalsi

--- zkouska/scriptukol.js
+++ zkouska/scriptukol.js
@@ -1,3 +1,7 @@
 const prijem = 12*174*15
 
-document.body.innerHTML = prijem
+document.body.innerHTML += prijem
+
+const prijem2 = prijem*0.6+prijem*0.65*0.4
+
+document.body.innerHTML += "<p>" + (prijem2) + "</p>"
